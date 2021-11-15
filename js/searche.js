8be51cc02ddf429306910ca27e26547e75bf8b71
Samir-isp <samirlounis2018@gmail.com>
2021-11-15 21:15:57 +0100

    d

--- js/searche.js
+++ js/searche.js
@@ -7,20 +7,20 @@ function closeSearch() {
 }
 
 function filterContent(elmnt) {
-  var x, i, val, show, showh2, showbr;
+  var x, i, val, show, showh3, showbr;
   val = elmnt.value;
   x = document.getElementById("leftmenuinnerinner").getElementsByTagName("*");
   for (i = 1; i < x.length; i++) {
     if (x[i].tagName == "H2") {
-      if (showh2) {
+      if (showh3) {
         if (show == false) {
-          showh2.style.display = "none";
+          showh3.style.display = "none";
         } else {
-          showh2.style.display = "block";
+          showh3.style.display = "block";
         }
       }
       show = false;
-      showh2 = x[i];
+      showh3 = x[i];
     }
     if (x[i].tagName == "BR") {
       if (showbr) {
@@ -40,11 +40,11 @@ function filterContent(elmnt) {
         show = true;
       }
     }
-    if (showh2) {
+    if (showh3) {
       if (show == false) {
-        showh2.style.display = "none";
+        showh3.style.display = "none";
       } else {
-        showh2.style.display = "block";
+        showh3.style.display = "block";
       }
     }      
   }
